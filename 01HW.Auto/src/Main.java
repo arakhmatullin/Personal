e2e57e216128695915e3575dba178dd582096d1f
Azat Rakhmatullin <arakhmatullin@cloudally.com>
2019-09-17 00:28:16 +0300

    commit

--- 01HW.Auto/src/Main.java
+++ 01HW.Auto/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         double fuelPerHundred = 9.6;
-        double fuelCount = 60;
-        if (fuelPerHundred > 0 && fuelCount > 0) {
-            int expectedDistance = (int) (fuelCount / fuelPerHundred * 100);
+        int fuelVolume = 60;
+        if (fuelPerHundred > 0 && fuelVolume > 0) {
+            int expectedDistance = (int) (fuelVolume / fuelPerHundred * 100);
             System.out.println(expectedDistance);
         } else {
             System.out.println("You have made error in source parameters");
