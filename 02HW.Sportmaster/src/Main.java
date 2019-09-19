4e0cb98322ecf51e109925ac310ff03c470aef44
Azat Rakhmatullin <arakhmatullin@cloudally.com>
2019-09-19 10:47:29 +0300

    HW02

--- 02HW.Sportmaster/src/Main.java
+++ 02HW.Sportmaster/src/Main.java
@@ -3,7 +3,10 @@ public class Main {
 
         int sumOfPrevBuys = 100000;
         int sumOfCurrentBuy = 3000;
-
+        if (sumOfPrevBuys < 0 && sumOfCurrentBuy <= 0) {
+            System.out.println("You have made mistake in source parameters.");
+            return 1;
+        }
         int bonusChunk = 1000;
         int stdBonus = 50;
         int silverBonus = 70;
